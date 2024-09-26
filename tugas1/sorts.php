a2efefcad7058b06cf57862c5d8d56b662db9489
Andrey Hartawan Suwardi <144876604+KingPublic@users.noreply.github.com>
2024-09-26 16:08:40 +0800

    SortArray

--- tugas1/sorts.php
+++ tugas1/sorts.php
@@ -49,13 +49,13 @@
 <body>
     <form method="post">
         <label for="nums1">Masukkan Array Nums1 (pisahkan dengan koma):</label>
-        <input type="text" name="nums1" id="nums1" required>
+        <input type="text" name="nums1" id="nums1" >
         <label for="m">Jumlah elemen aktif di Nums1:</label>
-        <input type="number" name="m" id="m" required>
+        <input type="number" name="m" id="m" >
         <label for="nums2">Masukkan Array Nums2 (pisahkan dengan koma):</label>
-        <input type="text" name="nums2" id="nums2" required>
+        <input type="text" name="nums2" id="nums2" >
         <label for="n">Jumlah elemen di Nums2:</label>
-        <input type="number" name="n" id="n" required>
+        <input type="number" name="n" id="n" >
         <input type="submit" value="Gabungkan">
     </form>
 
@@ -66,6 +66,8 @@
         $nums2 = array_map('intval', explode(',', $_POST['nums2']));
         $n = intval($_POST['n']);
 
+        
+
         // Memastikan nums1 cukup besar untuk menampung nums2
         $nums1 = array_slice($nums1, 0, $m); // Memotong array untuk menghilangkan nol tambahan
         $nums1 = array_merge($nums1, array_fill(0, $n, 0)); // Menambahkan nol untuk ruang nums2
@@ -75,22 +77,38 @@
         echo "<div class='containerarray'>Array gabungan: <br><pre>";
         print_r($nums1);
         echo "</pre></div>";
+
+        echo "<div class='containerarray'>Array gabungan tanpa indeks: <br>";
+        echo "[" . implode(', ', $nums1) . "]";
+        echo "</div>";
     }
 
-    function merge(&$nums1, $m, $nums2, $n) {
-        $index1 = $m - 1;
-        $index2 = $n - 1;
-        $mergeIndex = $m + $n - 1;
+    if ((empty($_POST['nums1']) && intval($_POST['m']) > 0) || (empty($_POST['nums2']) && intval($_POST['n']) > 0)) {
+        echo "<script>alert('Peringatan: Array Nums1 atau Nums2 kosong tetapi jumlah elemen diisi lebih dari 0.');</script>";
+    }
 
-        while ($index2 >= 0) {
-            if ($index1 >= 0 && $nums1[$index1] > $nums2[$index2]) {
-                $nums1[$mergeIndex] = $nums1[$index1];
-                $index1--;
+    
+
+    function merge(&$nums1, $m, $nums2, $n) {
+        $i = $m - 1;
+        $j = $n - 1;
+        $k = $m + $n - 1;
+    
+        while ($i >= 0 && $j >= 0) {
+            if ($nums1[$i] > $nums2[$j]) {
+                $nums1[$k] = $nums1[$i];
+                $i--;
             } else {
-                $nums1[$mergeIndex] = $nums2[$index2];
-                $index2--;
+                $nums1[$k] = $nums2[$j];
+                $j--;
             }
-            $mergeIndex--;
+            $k--;
+        }
+    
+        while ($j >= 0) {
+            $nums1[$k] = $nums2[$j];
+            $j--;
+            $k--;
         }
     }
     ?>
